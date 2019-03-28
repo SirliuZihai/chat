59a0d9f7e342e156b802903a82051bd87be15b50
sirliu梓海 <liuyizhi805@163.com>
2019-03-29 02:09:05 +0800

    @ServletComponentScan

--- src/main/java/com/zihai/App.java
+++ src/main/java/com/zihai/App.java
@@ -3,14 +3,15 @@ package com.zihai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-
+@ServletComponentScan  //使WebFilter生效 不然全部拦截（同时去掉@Conponent）
 @SpringBootApplication
 //(exclude= {DataSourceAutoConfiguration.class})
-public class Example {
-
+public class App {
+	
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Example.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
